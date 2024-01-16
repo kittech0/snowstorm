a51edc7c116d3181a8f573c0a4d0cb437f8233a7
kittech <kittech@skiff.com>
2024-01-16 17:33:11 +0100

    Added colors Renamed arguments air -> space, snowflake -> particle Distribution now can take floats

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,49 @@ use clap::Parser;
 use crate::terminal::Terminal;
 
 mod terminal;
-
+/// Click ESC to exist
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // 0 - 255
-    #[arg(short, long, default_value="50")]
-    distribution: u8,
+    /// 0 - 100 (float)
+    #[arg(short='d', long, default_value="20")]
+    distribution: f64,
 
-    // Speed in milliseconds (Smaller -> faster)
-    #[arg(short='S', long, default_value="1000")]
+    /// Speed in milliseconds (Smaller -> faster)
+    #[arg(short='z', long, default_value="1000")]
     speed: u64,
 
-    #[arg(short, long, default_value="*")]
-    snowflake: char,
+    /// Particle character
+    #[arg(short= 'p', long, default_value="*")]
+    particle: char,
 
-    #[arg(short, long, default_value=" ")]
-    air: char,
+    /// Space character
+    #[arg(short='s', long, default_value=" ")]
+    space: char,
 
-    #[arg(short = 'B', long)]
+    /// Space between particles
+    #[arg(short= 'B', long)]
     space_between: bool,
+
+    /// Particle color (ANSI)
+    #[arg(short= 'P', long)]
+    particle_color: Option<u8>,
+
+    /// Background color (ANSI)
+    #[arg(short= 'c', long)]
+    background_color: Option<u8>,
+
+    /// Space color (ANSI)
+    #[arg(short= 'S', long)]
+    space_color: Option<u8>
 }
 
 fn main()  {
     let args = Args::parse();
     let out = BufWriter::new(stdout());
     let (width, height) = crossterm::terminal::size().expect("Failed to get terminal size");
-    let mut terminal = Terminal::new(out, args, height as usize, width as usize);
+    let mut terminal = Terminal::new(out, args, height, width);
 
-    terminal.setup();
     loop {
         terminal.event_loop();
         terminal.main_loop();
--- src/terminal.rs
+++ src/terminal.rs
@@ -4,44 +4,33 @@ use std::thread;
 use std::time::Duration;
 use crossterm::event::{Event, KeyCode, poll, read};
 use crossterm::{cursor, execute, queue};
-use crossterm::cursor::{MoveLeft, MoveToRow};
-use crossterm::style::Print;
+use crossterm::cursor::{MoveToColumn, MoveToRow};
+use crossterm::style::Color::Reset;
+use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, StyledContent, Stylize};
 use crossterm::terminal::{BeginSynchronizedUpdate, Clear, ClearType, disable_raw_mode, DisableLineWrap, enable_raw_mode, EnableLineWrap, EndSynchronizedUpdate, EnterAlternateScreen, LeaveAlternateScreen, ScrollDown, SetTitle};
 use rand::{Rng, thread_rng};
 use crate::Args;
 
 pub struct Terminal<T: Write> {
     out: T,
-    height: usize,
-    width: usize,
+    height: u16,
+    width: u16,
     args: Args,
     chance: f64,
 }
 
 impl<T: Write> Terminal<T> {
-    pub fn new(out: T, args: Args, height: usize, width: usize) -> Self {
-        let chance = (args.distribution as f64) / 255.0;
-        Terminal { out, height, width, args, chance}
-    }
-
-    pub fn setup(&mut self) {
-        enable_raw_mode().expect("Failed to enable raw mode");
-        queue!(self.out,EnterAlternateScreen).expect("Failed to enter alternate screen");
-        queue!(self.out,DisableLineWrap).expect("Failed to disable line wrap");
-        queue!(self.out,Clear(ClearType::All)).expect("Failed to clear screen");
-        queue!(self.out,cursor::Hide).expect("Failed to hide cursor");
-        queue!(self.out,SetTitle("Snowstorm")).expect("Failed to set title");
-        queue!(self.out,MoveToRow(0)).expect("Failed to move cursor");
-        queue!(self.out,BeginSynchronizedUpdate).expect("Failed to begin synchronized update");
-        self.out.flush().expect("Failed to flush");
+    pub fn new(out: T, args: Args, height: u16, width: u16) -> Self {
+        let chance = args.distribution / 100.0;
+        let mut terminal = Terminal { out, height, width, args, chance};
+        terminal.setup();
+        terminal
     }
 
     pub fn main_loop(&mut self) {
-        let line = self.generate_line();
-        let size = line.len();
         queue!(self.out, ScrollDown(1)).expect("Failed to move cursor");
-        queue!(self.out, Print(line)).expect("Failed to write");
-        queue!(self.out, MoveLeft(size as u16)).expect("Failed to move cursor");
+        self.queue_chars();
+        queue!(self.out, MoveToColumn(0)).expect("Failed to move cursor");
         self.out.flush().expect("Failed to flush");
         thread::sleep(Duration::from_millis(self.args.speed));
     }
@@ -50,11 +39,11 @@ impl<T: Write> Terminal<T> {
         if let Ok(true) = poll(Duration::from_millis(5)) {
             match read().expect("Failed to read event") {
                 Event::Resize(width, height) => {
-                    if self.height < height as usize || self.width < width as usize {
+                    if self.width < width {
                         execute!(self.out,Clear(ClearType::All)).expect("Failed to clear screen");
                     }
-                    self.height = height as usize;
-                    self.width = width as usize;
+                    self.height = height;
+                    self.width = width;
                 }
                 Event::Key(event) => {
                     if event.code == KeyCode::Esc {
@@ -67,6 +56,21 @@ impl<T: Write> Terminal<T> {
         }
     }
 
+    fn setup(&mut self) {
+        enable_raw_mode().expect("Failed to enable raw mode");
+        queue!(self.out,EnterAlternateScreen).expect("Failed to enter alternate screen");
+        queue!(self.out,DisableLineWrap).expect("Failed to disable line wrap");
+        queue!(self.out,Clear(ClearType::All)).expect("Failed to clear screen");
+        queue!(self.out,cursor::Hide).expect("Failed to hide cursor");
+        queue!(self.out,SetTitle("Snowstorm")).expect("Failed to set title");
+        queue!(self.out,MoveToRow(0)).expect("Failed to move cursor");
+        queue!(self.out,BeginSynchronizedUpdate).expect("Failed to begin synchronized update");
+        if let Some(color) = self.args.background_color {
+            queue!(self.out,SetBackgroundColor(Color::AnsiValue(color))).expect("Failed to set color");
+        }
+        self.out.flush().expect("Failed to flush");
+    }
+
     fn restore(&mut self) {
         disable_raw_mode().expect("Failed to disable raw mode");
         queue!(self.out,EnableLineWrap).expect("Failed to enable line wrap");
@@ -74,28 +78,40 @@ impl<T: Write> Terminal<T> {
         queue!(self.out,SetTitle("")).expect("Failed to set title");
         queue!(self.out,LeaveAlternateScreen).expect("Failed to leave alternate screen");
         queue!(self.out,EndSynchronizedUpdate).expect("Failed to begin synchronized update");
+        queue!(self.out,ResetColor).expect("Failed to reset color");
         self.out.flush().expect("Failed to flush");
     }
 
-    fn generate_line(&self) -> String {
-        let mut builder = String::with_capacity(self.width);
+    fn queue_chars(&mut self) {
+        let mut last_char = self.args.space;
         for _ in 0..self.width {
-            let last_char = builder.chars().last().unwrap_or(' ');
-            builder.push(self.gen_char(last_char));
+            let new_char = self.generate_char(last_char);
+            last_char = *new_char.content();
+            queue!(self.out, Print(&new_char)).expect("Failed to write");
         }
-        builder
     }
 
-    fn gen_char(&self, last_char: char) -> char {
+    fn generate_char(&self, last_char: char) -> StyledContent<char> {
         let random = thread_rng().gen_bool(self.chance);
+        let space_color = if let Some(color) = self.args.space_color {
+            Color::AnsiValue(color)
+        } else {
+            Reset
+        };
+        let particle_color = if let Some(color) = self.args.particle_color {
+            Color::AnsiValue(color)
+        } else {
+            Reset
+        };
+
         if random {
-            if self.args.space_between && last_char == self.args.snowflake {
-                self.args.air
+            if self.args.space_between && last_char == self.args.particle {
+                self.args.space.with(space_color)
             } else {
-                self.args.snowflake
+                self.args.particle.with(particle_color)
             }
         } else {
-            self.args.air
+            self.args.space.with(space_color)
         }
     }
 }
